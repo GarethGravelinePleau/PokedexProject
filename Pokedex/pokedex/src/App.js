0c1677377776c5af71edbc4dbfebb76148d3fc2b
Gareth Graveline-Pleau <gherring@alumni.uoguelph.ca>
2022-02-16 21:28:56 -0500

    done for tonight

---
+++ Pokedex/pokedex/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import AppNavigator from './components/AppNavigator'
+import Pokedex from './containers/Pokedex'
+
+export default function App() {
+  return (
+   <Router>
+     <AppNavigator/>
+     <Route path="/" component={Pokedex}/>
+   </Router>
+  )
+}
+
